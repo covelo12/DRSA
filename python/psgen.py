925f6ea87ef196e13f3eca7a7bc232684b550db3
covelo12 <ricardocovelo11@ua.pt>
2023-11-18 10:07:34 +0000

    done loop iterations

--- python/psgen.py
+++ python/psgen.py
@@ -2,6 +2,7 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA512
 from Crypto.Random import get_random_bytes
 
+
 #Returns the index right after the confusion string and the result are equal
 def getIndex( confString, result): 
     leng= len(confString.hex())
@@ -10,20 +11,25 @@ def getIndex( confString, result):
         i = i + 1
     return i+leng
 
-def getResult(confString, result):
+def getResult(password, confString, result):
+    trys = 1000
     while ( str(confString.hex()) not in  str(result.hex()) ):
         trys = trys + 500
         result = PBKDF2(password, confString, trys, count=iterations, hmac_hash_module=SHA512)
     return result
 
-iterations = 1000
-password = "teste"
-confString = b'12'
-trys = 1000
+iterations = int(input("Number of iterations "))
+password = input("Password ")
+confString = input("Confusion String ").encode()
+#confString = "13".encode()
 
+final=b''
+result = b''
 
-result = PBKDF2(password, confString, trys, count=iterations, hmac_hash_module=SHA512)
-result= getResult(confString,result)
-index=  getIndex(confString, result)
-
-print(index)
+for(k) in range(0, iterations):   
+    result= getResult(password,confString,result) 
+    index=  getIndex(confString, result)
+    final+= result[:index+1]
+    password = PBKDF2(password, confString, index+len(password), count=iterations, hmac_hash_module=SHA512)[index:]
+    final+= getResult(password,confString,result)
+print(final.hex())
