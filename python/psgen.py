d5f3ec8457528824d2a9e6befcc2a89293a33782
covelo12 <ricardocovelo11@ua.pt>
2023-11-18 09:11:02 +0000

    defenition of the getResult funciotn

--- python/psgen.py
+++ python/psgen.py
@@ -10,6 +10,12 @@ def getIndex( confString, result):
         i = i + 1
     return i+leng
 
+def getResult(confString, result):
+    while ( str(confString.hex()) not in  str(result.hex()) ):
+        trys = trys + 500
+        result = PBKDF2(password, confString, trys, count=iterations, hmac_hash_module=SHA512)
+    return result
+
 iterations = 1000
 password = "teste"
 confString = b'12'
@@ -17,10 +23,7 @@ trys = 1000
 
 
 result = PBKDF2(password, confString, trys, count=iterations, hmac_hash_module=SHA512)
-
-while ( str(confString.hex()) not in  str(result.hex()) ):
-    trys = trys + 500
-    result = PBKDF2(password, confString, trys, count=iterations, hmac_hash_module=SHA512)
-
+result= getResult(confString,result)
 index=  getIndex(confString, result)
+
 print(index)
