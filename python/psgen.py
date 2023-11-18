ba66da1ef1dddf7129f4a3d64c47751b033d172c
covelo12 <ricardocovelo11@ua.pt>
2023-11-18 09:06:25 +0000

    defenition of the function getIdex

--- python/psgen.py
+++ python/psgen.py
@@ -2,6 +2,14 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA512
 from Crypto.Random import get_random_bytes
 
+#Returns the index right after the confusion string and the result are equal
+def getIndex( confString, result): 
+    leng= len(confString.hex())
+    i = 0
+    while (str(result.hex())[i:i+leng] != str(confString.hex())):
+        i = i + 1
+    return i+leng
+
 iterations = 1000
 password = "teste"
 confString = b'12'
@@ -14,8 +22,5 @@ while ( str(confString.hex()) not in  str(result.hex()) ):
     trys = trys + 500
     result = PBKDF2(password, confString, trys, count=iterations, hmac_hash_module=SHA512)
 
-len= len(confString.hex())
-i = 0
-while (str(result.hex())[i:i+len] != str(confString.hex())):
-    i = i + 1
-print(i)
+index=  getIndex(confString, result)
+print(index)
